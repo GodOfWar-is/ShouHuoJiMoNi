aa910ade5f9fb5dd9a4b8496829875312d140c30
GodOfWar-is <2515979610@qq.com>
2024-11-01 15:58:54 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ShouHuoJiMoNi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPanel = new System.Windows.Forms.Panel();
+            this.allButton = new System.Windows.Forms.Button();
+            this.RiHuaButton = new System.Windows.Forms.Button();
+            this.cakeButton = new System.Windows.Forms.Button();
+            this.XiuXianButton = new System.Windows.Forms.Button();
+            this.drinkButton = new System.Windows.Forms.Button();
+            this.milkButton = new System.Windows.Forms.Button();
+            this.productPanel = new System.Windows.Forms.Panel();
+            this.grandPanel = new System.Windows.Forms.Panel();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.suggestButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPanel.SuspendLayout();
+            this.grandPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonPanel
+            // 
+            this.buttonPanel.BackColor = System.Drawing.Color.Blue;
+            this.buttonPanel.Controls.Add(this.milkButton);
+            this.buttonPanel.Controls.Add(this.drinkButton);
+            this.buttonPanel.Controls.Add(this.XiuXianButton);
+            this.buttonPanel.Controls.Add(this.cakeButton);
+            this.buttonPanel.Controls.Add(this.RiHuaButton);
+            this.buttonPanel.Controls.Add(this.allButton);
+            this.buttonPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonPanel.Location = new System.Drawing.Point(0, 0);
+            this.buttonPanel.Name = "buttonPanel";
+            this.buttonPanel.Size = new System.Drawing.Size(780, 46);
+            this.buttonPanel.TabIndex = 0;
+            // 
+            // allButton
+            // 
+            this.allButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.allButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.allButton.ForeColor = System.Drawing.Color.White;
+            this.allButton.Location = new System.Drawing.Point(0, 0);
+            this.allButton.Name = "allButton";
+            this.allButton.Size = new System.Drawing.Size(120, 46);
+            this.allButton.TabIndex = 0;
+            this.allButton.Text = "全部";
+            this.allButton.UseVisualStyleBackColor = false;
+            // 
+            // RiHuaButton
+            // 
+            this.RiHuaButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.RiHuaButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.RiHuaButton.ForeColor = System.Drawing.Color.White;
+            this.RiHuaButton.Location = new System.Drawing.Point(120, 0);
+            this.RiHuaButton.Name = "RiHuaButton";
+            this.RiHuaButton.Size = new System.Drawing.Size(120, 46);
+            this.RiHuaButton.TabIndex = 1;
+            this.RiHuaButton.Text = "日化用品";
+            this.RiHuaButton.UseVisualStyleBackColor = false;
+            // 
+            // cakeButton
+            // 
+            this.cakeButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.cakeButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.cakeButton.ForeColor = System.Drawing.Color.White;
+            this.cakeButton.Location = new System.Drawing.Point(240, 0);
+            this.cakeButton.Name = "cakeButton";
+            this.cakeButton.Size = new System.Drawing.Size(120, 46);
+            this.cakeButton.TabIndex = 2;
+            this.cakeButton.Text = "蛋糕面包";
+            this.cakeButton.UseVisualStyleBackColor = false;
+            // 
+            // XiuXianButton
+            // 
+            this.XiuXianButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.XiuXianButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.XiuXianButton.ForeColor = System.Drawing.Color.White;
+            this.XiuXianButton.Location = new System.Drawing.Point(360, 0);
+            this.XiuXianButton.Name = "XiuXianButton";
+            this.XiuXianButton.Size = new System.Drawing.Size(120, 46);
+            this.XiuXianButton.TabIndex = 3;
+            this.XiuXianButton.Text = "休闲食品";
+            this.XiuXianButton.UseVisualStyleBackColor = false;
+            // 
+            // drinkButton
+            // 
+            this.drinkButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.drinkButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.drinkButton.ForeColor = System.Drawing.Color.White;
+            this.drinkButton.Location = new System.Drawing.Point(480, 0);
+            this.drinkButton.Name = "drinkButton";
+            this.drinkButton.Size = new System.Drawing.Size(120, 46);
+            this.drinkButton.TabIndex = 4;
+            this.drinkButton.Text = "饮品饮料";
+            this.drinkButton.UseVisualStyleBackColor = false;
+            // 
+            // milkButton
+            // 
+            this.milkButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.milkButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.milkButton.ForeColor = System.Drawing.Color.White;
+            this.milkButton.Location = new System.Drawing.Point(600, 0);
+            this.milkButton.Name = "milkButton";
+            this.milkButton.Size = new System.Drawing.Size(120, 46);
+            this.milkButton.TabIndex = 5;
+            this.milkButton.Text = "牛奶乳品";
+            this.milkButton.UseVisualStyleBackColor = false;
+            // 
+            // productPanel
+            // 
+            this.productPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.productPanel.Location = new System.Drawing.Point(0, 46);
+            this.productPanel.Name = "productPanel";
+            this.productPanel.Size = new System.Drawing.Size(780, 611);
+            this.productPanel.TabIndex = 1;
+            // 
+            // grandPanel
+            // 
+            this.grandPanel.BackColor = System.Drawing.Color.Blue;
+            this.grandPanel.Controls.Add(this.label1);
+            this.grandPanel.Controls.Add(this.button1);
+            this.grandPanel.Controls.Add(this.suggestButton);
+            this.grandPanel.Controls.Add(this.countdownLabel);
+            this.grandPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grandPanel.Location = new System.Drawing.Point(0, 657);
+            this.grandPanel.Name = "grandPanel";
+            this.grandPanel.Size = new System.Drawing.Size(780, 48);
+            this.grandPanel.TabIndex = 2;
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.AutoSize = true;
+            this.countdownLabel.Font = new System.Drawing.Font("宋体", 12F);
+            this.countdownLabel.ForeColor = System.Drawing.Color.White;
+            this.countdownLabel.Location = new System.Drawing.Point(26, 15);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(34, 24);
+            this.countdownLabel.TabIndex = 0;
+            this.countdownLabel.Text = "59";
+            // 
+            // suggestButton
+            // 
+            this.suggestButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.suggestButton.ForeColor = System.Drawing.Color.White;
+            this.suggestButton.Location = new System.Drawing.Point(460, 13);
+            this.suggestButton.Name = "suggestButton";
+            this.suggestButton.Size = new System.Drawing.Size(120, 30);
+            this.suggestButton.TabIndex = 1;
+            this.suggestButton.Text = "意见反馈";
+            this.suggestButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(612, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "购物车";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkOrange;
+            this.label1.Font = new System.Drawing.Font("宋体", 13F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(729, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 702);
+            this.Controls.Add(this.grandPanel);
+            this.Controls.Add(this.productPanel);
+            this.Controls.Add(this.buttonPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.buttonPanel.ResumeLayout(false);
+            this.grandPanel.ResumeLayout(false);
+            this.grandPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel buttonPanel;
+        private System.Windows.Forms.Button drinkButton;
+        private System.Windows.Forms.Button XiuXianButton;
+        private System.Windows.Forms.Button cakeButton;
+        private System.Windows.Forms.Button RiHuaButton;
+        private System.Windows.Forms.Button allButton;
+        private System.Windows.Forms.Button milkButton;
+        private System.Windows.Forms.Panel productPanel;
+        private System.Windows.Forms.Panel grandPanel;
+        private System.Windows.Forms.Label countdownLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button suggestButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
